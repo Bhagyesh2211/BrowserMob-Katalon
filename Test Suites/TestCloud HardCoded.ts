19bfd85061d77ff741eacf418ab6b26acdce6253
bhagyesh <bhagyesh@bhagyeshs-mbp.bbrouter>
2023-02-09 13:10:59 +0530

    Commit

--- Test Suites/TestCloud HardCoded.ts
+++ Test Suites/TestCloud HardCoded.ts
@@ -12,10 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>1ee2fe0b-fe9b-4eec-a983-5f48ef8a2af3</testSuiteGuid>
    <testCaseLink>
-      <guid>59409180-412e-481f-bb69-8f578c66778c</guid>
+      <guid>32db8ff0-012e-4140-bb84-a8a6b15d4159</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/TestCloud Tunnel</testCaseId>
+      <testCaseId>Test Cases/TestCloud HardCoded</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
